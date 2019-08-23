2a7221da33a0e281d10a8a66820fc5d40e93f48e
Yash-J <Yash-J@users.noreply.github.com>
2019-08-23 22:20:36 +0530

    Recipe list component
    Version :-2.0

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import {Recipe} from '../recipe.model';
-
+import {RecipeService} from '../recipe.service'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -8,13 +8,11 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 	@Output()recipeWasSelected=new EventEmitter<Recipe>();
-recipes:Recipe[]=[
-	new Recipe('Test Recipe 1','this is simply a test','https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg'),
-	new Recipe('Test Recipe 2','this is simply a test','https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg')
-];
-  constructor() { }
+recipes:Recipe[];
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit() {
+  	this.recipes=this.recipeService.getRecipes();
   }
 	onRecipeSelected(recipe:Recipe){
 		this.recipeWasSelected.emit(recipe);
