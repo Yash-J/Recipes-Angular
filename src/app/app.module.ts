fd5e0e904fc4a7e8cbd023a79611dad9918856be
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-11-09 17:55:25 +0530

    Added HttpClient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
@@ -9,13 +11,13 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import {ReactiveFormsModule,FormsModule} from '@angular/forms';
 import {DropdownDirective} from './shared/dropdown.directive';
-import {ShoppingListService} from '../app/shopping-list/shopping-list.service'
+import {ShoppingListService} from '../app/shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import{RecipeService} from './recipes/recipe.service';
+import {RecipeService} from './recipes/recipe.service';
+
 
 @NgModule({
   declarations: [
@@ -33,7 +35,7 @@ import{RecipeService} from './recipes/recipe.service';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,FormsModule,
+    ReactiveFormsModule, FormsModule, HttpClientModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService,RecipeService],
