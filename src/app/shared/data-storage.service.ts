742c90b8ef96a77a3167c00b50bc63221d34706e
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-11-10 21:17:35 +0530

    Changes to fetch Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -10,6 +11,13 @@ export class DataStorageService {
     this.http.put('https://recipes-project-3ce87.firebaseio.com/recipes.json', recipes)
     .subscribe(response => {
       console.log(response);
+    });
+  }
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://recipes-project-3ce87.firebaseio.com/recipes.json')
+    .subscribe(recipes => {
+      this.recipeService.setRecipes(recipes);
+
     });
   }
 }
