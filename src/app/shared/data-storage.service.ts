7910fc55a7e607240777df2e21c1ee65e61f65f5
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-11-19 08:53:20 +0530

    some changes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -14,10 +15,15 @@ export class DataStorageService {
     });
   }
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://recipes-project-3ce87.firebaseio.com/recipes.json')
-    .subscribe(recipes => {
-      this.recipeService.setRecipes(recipes);
+    return  this.http.get<Recipe[]>('https://recipes-project-3ce87.firebaseio.com/recipes.json')
+    .pipe(map(recipes => {
+      return recipes.map(recipe => {
+        return {...recipe, ingredients : recipe.ingredients ? recipe.ingredients : [] };
+      });
+    }),
+    tap(recipes => {this.recipeService.setRecipes(recipes);
+    })
+     )
 
-    });
   }
 }
