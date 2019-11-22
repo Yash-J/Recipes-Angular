164083464d514e294e139dfbee8bab3fbf13ae6b
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-11-22 08:39:41 +0530

    Displaying error message
    When same user tries to sign up , show an error message.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
   constructor(private authService: AuthService ) {}
 
   onSwitchMode() {
@@ -20,13 +22,17 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
-    if(this.isLoginMode){
+    this.isLoading = true;
+    if (this.isLoginMode) {
       //
     } else {
       this.authService.signup(email, password).subscribe(resData => {
         console.log(resData);
+        this.isLoading = false;
       }, error => {
         console.log(error);
+        this.error = 'An error occurred !';
+        this.isLoading = false;
       }
       );
     }
