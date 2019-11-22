af4dd7e3b62dab5fbdb2e37bcf15eab973f09ad1
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-11-22 09:00:48 +0530

    Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import {throwError} from 'rxjs';
 
 interface AuthResponseData {
   idToken: string;
@@ -16,8 +18,18 @@ export class AuthService {
     {
       email : email ,
       password : password ,
-      returnSecureoken : true
+      returnSecureToken : true
     }
-    );
+    ).pipe(catchError(errorRes => {
+      let errorMessage = 'An unnkown error occurred !';
+      if(!errorRes.error || !errorRes.error.error){
+        return throwError(errorMessage);
+      }
+      switch (errorRes.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email exists already';
+      }
+      return throwError(errorMessage);
+    }));
   }
 }
