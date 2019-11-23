4e1abe379a9eaababc8a4439650f0ce3cf310d11
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-11-23 17:32:13 +0530

    Added Error Handling logic for Login of user

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import {throwError} from 'rxjs';
 
@@ -21,24 +21,32 @@ export class AuthService {
       password : password ,
       returnSecureToken : true
     }
-    ).pipe(catchError(errorRes => {
-      let errorMessage = 'An unnkown error occurred !';
-      if(!errorRes.error || !errorRes.error.error){
-        return throwError(errorMessage);
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email exists already';
-      }
-      return throwError(errorMessage);
-    }));
+    ).pipe(catchError(this.handleError));
   }
   login(email: string, password: string) {
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCwYDuPiQKzGMR6yHcRDhfQvTOLfawFyM0',
       { email:email ,
        password : password ,
-      returnSecureToken : true});
+      returnSecureToken : true}).pipe(catchError(this.handleError));
 
+  }
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unnkown error occurred !';
+    if(!errorRes.error || !errorRes.error.error){
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+        case 'EMAIL_NOT_FOUND':
+            errorMessage = 'This email does not exist';
+            break;
+        case 'INVALID_PASSWORD':
+            errorMessage = 'This password is not correct';
+            break;
+    }
+    return throwError(errorMessage);
   }
 }
