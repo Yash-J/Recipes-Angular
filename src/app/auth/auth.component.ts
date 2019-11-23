901d671a0de3bd879a47da4f871fd0ca99dddab2
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-11-23 18:36:17 +0530

    Displaying different sections In User Interface for authenticated and unauthenticated users

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -11,7 +12,7 @@ export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
-  constructor(private authService: AuthService ) {}
+  constructor(private authService: AuthService, private router: Router ) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -34,7 +35,9 @@ export class AuthComponent {
     authObs.subscribe(resData => {
       console.log(resData);
       this.isLoading = false;
-    }, errorMessage => {
+      this.router.navigate(['/recipes']);
+    },
+    errorMessage => {
       console.log(errorMessage);
       this.error = errorMessage;
       this.isLoading = false;
