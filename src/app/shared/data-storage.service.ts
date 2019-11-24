d1bef2a0e2201bcd744b7bf57833255277b1d0f0
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-11-24 13:08:46 +0530

    Added token to outgoing requests.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -16,12 +16,8 @@ export class DataStorageService {
     });
   }
   fetchRecipes() {
-    return this.authService.user.pipe(take(1), exhaustMap(user => {
-      return  this.http.get<Recipe[]>('https://recipes-project-3ce87.firebaseio.com/recipes.json',
-      {
-        params: new HttpParams().set('auth', user.token)
-      });
-    }), map(recipes => {
+    return  this.http.get<Recipe[]>('https://recipes-project-3ce87.firebaseio.com/recipes.json',
+      ).pipe( map(recipes => {
       return recipes.map(recipe => {
         return {...recipe, ingredients : recipe.ingredients ? recipe.ingredients : [] };
       });
