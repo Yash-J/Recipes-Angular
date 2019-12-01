acfd520d65edcbce1e391b3ca52d1b00d30049bf
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-12-01 22:30:58 +0530

    Adding core module
    The core module consists of the services such as ShoppingListService, RecipeService. Also, it consists of the Auth Interceptor Service. The core module is added in the imports of App Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ReactiveFormsModule, FormsModule} from '@angular/forms';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListService} from '../app/shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
-import {RecipeService} from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 
 @NgModule({
@@ -23,13 +21,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     ReactiveFormsModule, FormsModule, HttpClientModule,
-    AppRoutingModule, RecipesModule, ShoppingListModule, SharedModule
-  ],
-  providers: [ShoppingListService, RecipeService, {
-    provide : HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true
-  }
+    AppRoutingModule, RecipesModule, ShoppingListModule, SharedModule, CoreModule
   ],
   bootstrap: [AppComponent]
 
