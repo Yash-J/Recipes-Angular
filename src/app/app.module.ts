b7568d8584a4b6fd2b3c939f79b82d36265bedb7
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-12-01 21:16:33 +0530

    Added the shared module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from '../app/shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
-import {PlaceholderDirective} from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule, FormsModule, HttpClientModule,
-    AppRoutingModule, RecipesModule, ShoppingListModule
+    AppRoutingModule, RecipesModule, ShoppingListModule, SharedModule
   ],
   providers: [ShoppingListService, RecipeService, {
     provide : HTTP_INTERCEPTORS,
@@ -39,7 +31,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     multi: true
   }
   ],
-  bootstrap: [AppComponent],
-  entryComponents: [AlertComponent]
+  bootstrap: [AppComponent]
+
 })
 export class AppModule { }
