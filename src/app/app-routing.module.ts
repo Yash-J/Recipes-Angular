4e727de63d85e53c57fd72a324281eb5e1ac9261
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-12-01 22:43:22 +0530

    Split the app module to various feature modules.
    Created Auth Module, which includes the Auth Component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { AuthComponent } from './auth/auth.component';
 import { RecipesRoutingModule } from './recipes/recipes-routing.module';
 
 const appRoutes: Routes = [
-	{path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'auth', component: AuthComponent}
+	{path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ];
 
 @NgModule({
