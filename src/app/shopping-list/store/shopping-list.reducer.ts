ada8bc942fbac242c45bd7c77d42046d25049951
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-12-08 16:29:11 +0530

    Added code to edit, delete and add all ingredients

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,3 +1,4 @@
+import {Action} from '@ngrx/store';
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from '../store/shopping-list.actions';
 
@@ -7,14 +8,37 @@ const initialState = {
     new Ingredient('Bananas', 2),
     ]
 };
-export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient ) {
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions ) {
   switch ( action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action.payload]
       };
-      default:
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
+      };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload.ingredient
+      };
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[action.payload.index] = updatedIngredient;
+      return {
+          ...state , ingredients: updatedIngredients
+        };
+    case ShoppingListActions.DELETE_INGREDIENT:
+          return {
+            ...state,
+            ingredients: state.ingredients.filter((ig, igIndex ) => {
+              return igIndex !== action.payload;
+            })
+          };
+    default:
         return state;
   }
 
