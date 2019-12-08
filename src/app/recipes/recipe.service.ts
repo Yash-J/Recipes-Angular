83cad6d7ffde609f80caac2b9f7429ebf5c1697e
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-12-08 23:15:21 +0530

    Changes in Recipe Service.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,7 +4,7 @@ import {Subject} from 'rxjs';
 import {Recipe} from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
 import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
-import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
+import * as fromApp from '../store/app.reducer';
 
 @Injectable()
 export class RecipeService {
@@ -22,7 +22,7 @@ export class RecipeService {
 // 		])
 // ];
 private recipes: Recipe[] = [];
-  constructor(private store: Store< fromShoppingList.AppState >) {}
+  constructor(private store: Store< fromApp.AppState >) {}
   setRecipes(recipes: Recipe [] ) {
     this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
