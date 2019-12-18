d99bf3d9d70a1fab92178c9fdd7ec1f39eb01aaf
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-12-18 19:22:21 +0530

    Adding, updating and deleting Recipes using NgRx and Store

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Recipe } from './recipe.model';
+import {Actions, ofType} from '@ngrx/effects';
+import { take, map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { Recipe } from './recipe.model';
 import * as fromApp from '../store/app.reducer';
 import * as RecipeActions from '../recipes/store/recipe.actions';
-import {Actions, ofType} from '@ngrx/effects';
-import { take } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolverService implements Resolve < Recipe[] > {
@@ -19,7 +20,16 @@ export class RecipesResolverService implements Resolve < Recipe[] > {
   //   }
   // }
   resolve( route: ActivatedRouteSnapshot , state: RouterStateSnapshot) {
-     this.store.dispatch(new RecipeActions.FetchRecipes());
-     return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
+    return this.store.select('recipes').pipe(take(1),map( recipesState => {
+      return recipesState.recipes;
+    }), switchMap( recipes => {
+        if (recipes.length === 0) {
+          this.store.dispatch(new RecipeActions.FetchRecipes());
+          return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
+        } else {
+          return of(recipes);
+        }
+    })
+    );
   }
 }
