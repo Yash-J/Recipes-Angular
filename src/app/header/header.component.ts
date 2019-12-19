038fa13cd877f0ffc3142e6152c59cdf17169371
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-12-19 23:05:17 +0530

    Clean up

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
-import { DataStorageService } from '../shared/data-storage.service';
-import { AuthService } from '../auth/auth.service';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
 import * as RecipeActions from '../recipes/store/recipe.actions';
@@ -18,9 +16,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private userSub: Subscription;
   isAuthenticated = false;
 
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService, private store: Store<fromApp.AppState> ) {}
+  constructor(private store: Store<fromApp.AppState> ) {}
   onSaveData() {
-    this.dataStorageService.storeRecipes();
+   // this.dataStorageService.storeRecipes();
+   this.store.dispatch(new RecipeActions.StoreRecipes());
   }
   onFetchData() {
   //  this.dataStorageService.fetchRecipes().subscribe();
