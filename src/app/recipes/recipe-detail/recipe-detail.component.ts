99a6567063049b2b770e631d2c6ea0760133ba40
Yash Jahagirdar <Yash-J@users.noreply.github.com>
2019-12-19 23:24:02 +0530

    Deleted Recipe Service References

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Recipe} from '../recipe.model';
-import {RecipeService} from '../recipe.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../../store/app.reducer';
 import { map, switchMap } from 'rxjs/operators';
 import * as RecipesActions from '../store/recipe.actions';
+import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -16,7 +16,6 @@ export class RecipeDetailComponent implements OnInit {
 recipe: Recipe;
 id: number;
   constructor(
-    private recipeService: RecipeService,
     private route: ActivatedRoute,
     private router: Router,
     private store: Store<fromApp.AppState>) { }
@@ -33,7 +32,8 @@ id: number;
   })).subscribe(recipe => {this.recipe = recipe; });
   }
   onAddToShoppingList() {
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  //  this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  this.store.dispatch(new ShoppingListActions.AddIngredients(this.recipe.ingredients));
   }
   onEditRecipe() {
     // this.router.navigate(['edit'], { relativeTo: this.route });
